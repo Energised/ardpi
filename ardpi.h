4cbc5a9bff3a6628ecfa2fd188242a4fbc91ee9d
Daniel Woolsey <woolsey64@gmail.com>
2015-04-13 13:12:32 +0100

    Update ardpi.h

--- ardpi.h
+++ ardpi.h
@@ -20,21 +20,21 @@
 		> motors.stop();
 
 */
-#ifndef ardpi_h
+#ifndef ardpi_h // defines the use of ardpi.h as a library
 #define ardpi_h
 
-#include "Arduino.h"
+#include "Arduino.h" // use the base arduino system
 
-class motors
+class motors // sets up the information for the motors class
 {
 public:
-	motors(int m1a, int m1b, int m2a, int m2b);
+	motors(int m1a, int m1b, int m2a, int m2b); // defines the methods for the class
 	void forwards(int x);
 	void backwards(int x);
 	void left(int x);
 	void right(int x);
 	void stop();
-private:
+private: // lists private variables
 	int _m1a;
 	int _m1b;
 	int _m2a;
