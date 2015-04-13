405d3c7b8cbe64986852b738389fed3f4fbf700c
Daniel Woolsey <woolsey64@gmail.com>
2015-04-13 12:49:50 +0100

    Update ardpi.cpp

--- ardpi.cpp
+++ ardpi.cpp
@@ -4,56 +4,56 @@
 	Released into the public domain
 */
 
-#include "Arduino.h"
-#include "ardpi.h"
+#include "Arduino.h" // use the Arduino header file
+#include "ardpi.h" // include the arpi header file
 
-motors::motors(int m1a, int m1b, int m2a, int m2b)
+motors::motors(int m1a, int m1b, int m2a, int m2b) // setup a motors class with 4 pins
 {
-    pinMode(m1a, OUTPUT);
+    pinMode(m1a, OUTPUT); // enable each pin
     pinMode(m1b, OUTPUT);
     pinMode(m2a, OUTPUT);
     pinMode(m2b, OUTPUT);
-    _m1a = m1a;
+    _m1a = m1a; // set private variables for the class
     _m1b = m1b;
     _m2a = m2a;
     _m2b = m2b;
 }
 
-void motors::forwards(int x)
+void motors::forwards(int x) // method to set forward bias
 {
-	analogWrite(_m1a, x);
+	analogWrite(_m1a, x); // writing correct polarity values for each pin based on time x
 	analogWrite(_m1b, 0);
 	analogWrite(_m2a, x);
 	analogWrite(_m2b, 0);
 }
 
-void motors::backwards(int x)
+void motors::backwards(int x) // method to set reverse bias
 {
-	analogWrite(_m1a, 0);
+	analogWrite(_m1a, 0); // writing correct polarities for each pin based on time x
 	analogWrite(_m1b, x);
 	analogWrite(_m2a, 0);
 	analogWrite(_m2b, x);
 }
 
-void motors::left(int x)
+void motors::left(int x) // method to set left bias
 {
-	analogWrite(_m1a, 0);
+	analogWrite(_m1a, 0); // writing correct polarities to each pin based on time x
 	analogWrite(_m1b, x);
 	analogWrite(_m2a, x);
 	analogWrite(_m2b, 0);
 }
 
-void motors::right(int x)
+void motors::right(int x) // method to set right bias
 {
-	analogWrite(_m1a, x);
+	analogWrite(_m1a, x); // writing correct polarities to each pin based on time x
 	analogWrite(_m1b, 0);
 	analogWrite(_m2a, 0);
 	analogWrite(_m2b, x);
 }
 
-void motors::stop()
+void motors::stop() // method to stop the motors
 {
-	analogWrite(_m1a, 0);
+	analogWrite(_m1a, 0); // writes stop signals to the motor pins
 	analogWrite(_m1b, 0);
 	analogWrite(_m2a, 0);
 	analogWrite(_m2b, 0);
